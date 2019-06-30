a6ea39a6f6714d56caac11c189d76bc568d3c505
Chazza <me@chazza.net>
2019-06-30 20:40:55 +0100

    Base commit

---
+++ src/main/java/gg/plugins/piggybanks/PiggyBanks.java
@@ -0,0 +1,16 @@
+package gg.plugins.piggybanks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PiggyBanks extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
